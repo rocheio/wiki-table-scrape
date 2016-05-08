736181220c6a601ef072ab9a8d51d87c023339d9
Andy Roche <andy@roche.io>
2016-05-08 13:57:11 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# wiki-table-scrape
-Scrape tables from Wikipedia articles as CSVs
+# Wikitablescrape
+
+Scrape all tables from a Wikipedia article into CSV files on your computer.
+
+To run the script, you must have [Python3][python] and the [Beautiful Soup][beautiful-soup] package installed. You can install Beautiful Soup from pip using `pip install bs4`.
+
+Replace the values of `OUTPUT_NAME` and `WIKI_URL` at the top of the script with the values you want. All large tables from the article at `WIKI_URL` will be downloaded to multiple CSV files in a folder named 'OUTPUT_NAME' in the same directory as your script.
+
+The script won't give you 100% clean data for every page on Wikipedia, but it will get you most of the way there and handle the three topics I mention in [my blog post][blog-post]. You can see the output from the pages for [mountain height][wiki-mountains], [volcano height][wiki-volcano] [NBA scores][wiki-nba], and [the highest-grossing films][wiki-films] in the `output` folder.
+
+Please open an Issue or Pull Request if you find a bug or have suggestions for improvement.
+
+[beautiful-soup]: https://www.crummy.com/software/BeautifulSoup/
+[blog-post]: https://roche.io/data/2016/05/08/scrape-wikipedia-into-csv.html
+[python]: https://www.python.org/downloads/
+[wiki-films]: https://en.wikipedia.org/wiki/List_of_highest-grossing_films
+[wiki-mountains]: https://en.wikipedia.org/wiki/List_of_mountains_by_elevation
+[wiki-nba]: https://en.wikipedia.org/wiki/List_of_National_Basketball_Association_career_scoring_leaders
+[wiki-volcano]: https://en.wikipedia.org/wiki/List_of_volcanoes_by_elevation
---
+++ wikitablescrape.py
@@ -0,0 +1,114 @@
+"""Create CSVs from all tables on a Wikipedia article."""
+
+import os
+import requests
+from bs4 import BeautifulSoup
+
+OUTPUT_NAME = "mountains"
+WIKI_URL = "https://en.wikipedia.org/wiki/List_of_mountains_by_elevation"
+
+
+def scrape():
+    """Create CSVs from all tables in a Wikipedia article."""
+
+    # Read tables from Wikipedia article into list of HTML strings
+    req = requests.get(WIKI_URL)
+    soup = BeautifulSoup(req.content, 'lxml')
+    table_classes = {"class": ["sortable", "plainrowheaders"]}
+    wikitables = soup.findAll("table", table_classes)
+
+    # Create folder for output if it doesn't exist
+    try:
+        os.mkdir(OUTPUT_NAME)
+    except Exception:
+        # Generic OS Error
+        pass
+
+    for index, table in enumerate(wikitables):
+        # Make a unique file name for each CSV
+        if index == 0:
+            filename = OUTPUT_NAME
+        else:
+            filename = OUTPUT_NAME + '_' + str(index)
+
+        filepath = os.path.join(OUTPUT_NAME, filename) + '.csv'
+
+        with open(filepath, 'w') as output:
+            write_html_table_to_csv(table, output)
+
+
+def write_html_table_to_csv(table, output):
+    """Write HTML table from Wikipedia to a CSV file."""
+
+    # Hold elements that span multiple rows in a list of
+    # dictionaries that track 'rows_left' and 'value'
+    saved_rowspans = []
+    for row in table.findAll("tr"):
+        cells = row.findAll(["th", "td"])
+
+        # If the first row, use it to define width of table
+        if len(saved_rowspans) == 0:
+            saved_rowspans = [None for _ in cells]
+        # Insert values from cells that span into this row
+        elif len(cells) != len(saved_rowspans):
+            for index, rowspan_data in enumerate(saved_rowspans):
+                if rowspan_data is not None:
+                    # Insert the data from previous row; decrement rows left
+                    value = rowspan_data['value']
+                    cells.insert(index, value)
+
+                    if saved_rowspans[index]['rows_left'] == 1:
+                        saved_rowspans[index] = None
+                    else:
+                        saved_rowspans[index]['rows_left'] -= 1
+
+        # If an element with rowspan, save it for future cells
+        for index, cell in enumerate(cells):
+            if cell.has_key("rowspan"):
+                rowspan_data = {
+                    'rows_left': int(cell["rowspan"]),
+                    'value': cell,
+                }
+                saved_rowspans[index] = rowspan_data
+
+        if cells:
+            cleaned = clean_data(cells)
+            output.write(cleaned)
+
+
+def clean_data(row):
+    """Clean table row list from Wikipedia into a string for CSV."""
+
+    cleaned_cells = []
+
+    for cell in row:
+        # Strip references from the cell
+        references = cell.findAll("sup", {"class": "reference"})
+        if references:
+            for ref in references:
+                ref.extract()
+
+        # Strip sortkeys from the cell
+        sortkeys = cell.findAll("span", {"class": "sortkey"})
+        if sortkeys:
+            for ref in sortkeys:
+                ref.extract()
+
+        # Strip footnotes from text and join into a single string
+        text_items = cell.findAll(text=True)
+        no_footnotes = [text for text in text_items if text[0] != '[']
+        puretext = ''.join(no_footnotes)
+
+        # Replace non-breaking spaces with regular spaces and add quotes
+        puretext = puretext.replace('\xa0', ' ')
+        quoted = '"' + puretext + '"'
+
+        cleaned_cells += [quoted]
+
+    string = ', '.join(cleaned_cells) + '\n'
+    print(string)
+    return string
+
+
+if __name__ == '__main__':
+    scrape()
