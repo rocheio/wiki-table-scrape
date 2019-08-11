f4b952e2b9c5e52dd8dcc6e6934b6c2d72dae809
Andy Roche <andy@roche.io>
2019-08-11 19:49:44 -0400

    convert table-driven tests into self.assert methods

--- wikitablescrape/test_scrape.py
+++ wikitablescrape/test_scrape.py
@@ -8,26 +8,36 @@ import unittest
 from . import scrape
 
 
-class TestFiles(unittest.TestCase):
-    """Test parsing HTML into CSVs using saved files."""
+class TestParseTables(unittest.TestCase):
+    """Test parsing HTML into CSV tables using saved files."""
 
-    def test_parse_rows_from_table(self):
-        cases = [
-            ("testdata/colspan/input.html", "testdata/colspan/output.csv"),
-            ("testdata/linebreaks/input.html", "testdata/linebreaks/output.csv"),
-            ("testdata/mountains/input.html", "testdata/mountains/output.csv"),
-            ("testdata/rowspan/input.html", "testdata/rowspan/output.csv"),
-        ]
-        for testin, testout in cases:
-            with open(testin, "r") as htmlfile:
-                table = scrape.get_tables_from_html(htmlfile.read())[0]
-                got = scrape.parse_rows_from_table(table)
+    def assert_html_to_csv(self, testin, testout):
+        """Assert that an HTML input file becomes an HTML output file."""
+        with open(testin, "r") as htmlfile:
+            table = scrape.get_tables_from_html(htmlfile.read())[0]
+            got = scrape.parse_rows_from_table(table)
 
-            with open(testout, "r") as csvfile:
-                want = list(csv.reader(csvfile))
+        with open(testout, "r") as csvfile:
+            want = list(csv.reader(csvfile))
 
-            for g, w in zip(got, want):
-                self.assertEqual(g, w)
+        for g, w in zip(got, want):
+            self.assertEqual(g, w)
+
+    def test_parse_colspans(self):
+        """Confirm that a <th colspan> element becomes multiple CSV column cells."""
+        self.assert_html_to_csv("testdata/colspan/input.html", "testdata/colspan/output.csv")
+
+    def test_parse_rowspans(self):
+        """Confirm that a <th rowspan> element becomes multiple CSV row cells."""
+        self.assert_html_to_csv("testdata/rowspan/input.html", "testdata/rowspan/output.csv")
+
+    def test_parse_linebreaks(self):
+        """Confirm that <br /> elements are parsed out and replaced with spaces."""
+        self.assert_html_to_csv("testdata/linebreaks/input.html", "testdata/linebreaks/output.csv")
+
+    def test_quotes_and_smalls(self):
+        """Confirm that <small> elements are included and `"` quotes are escaped."""
+        self.assert_html_to_csv("testdata/mountains/input.html", "testdata/mountains/output.csv")
 
     def test_scrape_tables_from_text(self):
         with tempfile.TemporaryDirectory() as tmpdir:
@@ -51,38 +61,32 @@ class TestFiles(unittest.TestCase):
 
 class TestParseTableHeader(unittest.TestCase):
 
+    def assert_header_from_table(self, header, html):
+        """Assert that a given header is returned from a given HTML string."""
+        table = scrape.get_tables_from_html(html)[0]
+        got = scrape.parse_table_header(table)
+        self.assertEqual(got, header)
+
     def test_caption(self):
         """A table with a <caption> will always choose that."""
-        html = """
+        self.assert_header_from_table("Caption", """
             <h2>Header</h2>
             <table>
                 <caption>Caption</caption>
             </table>
-        """
-        table = scrape.get_tables_from_html(html)[0]
-        got = scrape.parse_table_header(table)
-        want = "Caption"
-        self.assertEqual(got, want)
+        """)
 
     def test_h2(self):
         """A table without a caption returns the preceeding <h2>."""
-        html = """
+        self.assert_header_from_table("Header", """
             <h2>Header</h2>
             <table></table>
-        """
-        table = scrape.get_tables_from_html(html)[0]
-        got = scrape.parse_table_header(table)
-        want = "Header"
-        self.assertEqual(got, want)
+        """)
 
     def test_h2_and_h3(self):
         """A table with an <h2> header will also check for <h3> subheaders."""
-        html = """
+        self.assert_header_from_table("Header - Subheader", """
             <h2>Header</h2>
             <h3>Subheader</h3>
             <table></table>
-        """
-        table = scrape.get_tables_from_html(html)[0]
-        got = scrape.parse_table_header(table)
-        want = "Header - Subheader"
-        self.assertEqual(got, want)
+        """)
